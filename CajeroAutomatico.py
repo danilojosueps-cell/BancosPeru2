608470d7de7fec55ec00031cc0efce012217aee4
danilojosueps-cell <danilojosueps@gmail.com>
2026-07-16 16:02:56 -0500

    Update CajeroAutomatico.py
    MODIFICACION DEL BANCO BBVA

--- CajeroAutomatico.py
+++ CajeroAutomatico.py
@@ -3,14 +3,14 @@
 # Autor: Danilo Peña 3ro SEC    
 # ===========================================
 
-saldo = 20000
-pin_correcto = "9012"
+saldo = 6900
+pin_correcto = "3456"
 
 print("===================================")
-print("     CAJERO BANCO INTERBANK          ")
+print("     CAJERO DEL BANCO BBVA          ")
 print("===================================")
 
-pin = input("Ingrese  PIN: ")
+pin = input("POR FAVOR INGRESE SU PIN: ")
 
 if pin == pin_correcto:
 
